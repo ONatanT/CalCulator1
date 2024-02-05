7a7cad67c0b2ac2bb7830c29f411e043073cf8bf
Orel-Natan <O.T.Natan@gmail.com>
2024-02-05 13:50:44 +0200

    bugfix

--- app/src/main/java/com/example/calculator1/MainActivity.java
+++ app/src/main/java/com/example/calculator1/MainActivity.java
@@ -52,16 +52,6 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public class MainActivity extends AppCompatActivity {
-
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
-        }
-//    lsakd;laks;dlka
-//
-    }
 
 //    String getResult(String data){
 //        try {
