81cfb3dcaac24ea5e82a146c99ded0a37632b265
Orel-Natan <O.T.Natan@gmail.com>
2024-02-05 13:02:57 +0200

    finalstep2

--- app/src/main/java/com/example/calculator1/MainActivity.java
+++ app/src/main/java/com/example/calculator1/MainActivity.java
@@ -51,6 +51,22 @@ public class MainActivity extends AppCompatActivity {
         //String str = getResult(dataToCalculate);
 
     }
+package com.example.calculator1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+    public class MainActivity extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+        }
+//    lsakd;laks;dlka
+//
+    }
 
 //    String getResult(String data){
 //        try {
